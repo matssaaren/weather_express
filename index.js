e6c2351dec61bb8d6b362361abc23a312a5269d5
Mats Saaren <mats.saaren@voco.ee>
2024-10-07 11:19:11 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const fetch = require('node-fetch');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 const key = '6e366b3ae34dc6bdd56794422668a907';
 let city = 'Tartu';
 
@@ -25,6 +28,23 @@ app.get('/', function (req, res) {
             });
         })
   
+});
+
+app.post('/', function(req, res) {
+    let city = req.body.cityname;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(response => response.json())
+    .then(data => {
+        let description = data.weather[0].description;
+        let temp = Math.round(parseFloat(data.main.temp) - 273.15);
+
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        });
+    });
 });
 
 app.listen(3000);
