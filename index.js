e9a52710766be107defe65d4141b4f3cf2fc56d8
Mats Saaren <mats.saaren@voco.ee>
2024-10-07 11:32:17 +0300

    Created API error handling

--- index.js
+++ index.js
@@ -24,13 +24,14 @@ const getWeatherDataPromise = (url) => {
                 let result = {
                     description: description,
                     city: city,
-                    temp: temp
+                    temp: temp,
+                    error: null
                 };
 
                 resolve(result);
             })
             .catch(error => {
-                reject(error);
+                reject(error)
             });
     });
 };
@@ -71,7 +72,10 @@ app.all('/', function(req, res) {
     getWeatherDataPromise(url)
         .then(data => {
             res.render('index', data);
-        });
+        })
+    .catch(error => {
+        res.render('index', {error: "Problem with getting data, try again"})
+    })
 });
 
 app.listen(3000);
